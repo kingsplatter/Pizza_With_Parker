045b47355992d07382f5f41af0b9ec3cddf415fc
Parker Erikson <parkererikson@maed-2.local>
2017-03-08 13:21:13 -0700

    man i did the thing and pizza did stuff man holy cow www.youtube.com/user/KanyeWestVEVO

---
+++ Pizza With Parker/pizza.swift
@@ -0,0 +1,26 @@
+//
+//  pizza.swift
+//  Pizza With Parker
+//
+//  Created by Parker Erikson on 3/8/17.
+//  Copyright © 2017 e. All rights reserved.
+//
+
+import Foundation
+
+class Pizza{
+    
+    var size: String
+    var toppings : [String]
+    var sauce : String
+    var cost : Double
+    var crust : String
+
+    init(size: String, toppings: [String], sauce : String, cost: Double, crust : String) {
+        self.size = size
+        self.toppings = toppings
+        self.sauce = sauce
+        self.cost = cost
+        self.crust = crust
+    }
+}
